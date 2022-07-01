60378ce32cf17bcebdb404914c90c587468fcea0
meal-and-menu-maker <thomas.stokovic@gmail.com>
2022-07-01 17:16:18 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -68,7 +68,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Macro Meal & Menu Maker</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="../public/favicon.ico" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"></link>
       </Head>
 

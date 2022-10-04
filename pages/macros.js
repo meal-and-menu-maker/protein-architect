68aee125a908ffc96d3b334b7456f4440001c7c8
protein-architect <tigretony8@gmail.com>
2022-10-04 14:49:42 +0200

    tests

--- pages/macros.js
+++ pages/macros.js
@@ -1261,35 +1261,35 @@ export default function Home() {
 
             /* Style the navbar */
             #navbar {
-            overflow: hidden;
-            background-color: #333;
+                overflow: hidden;
+                background-color: #f5f5dc;
             }
 
             /* Navbar links */
             #navbar a {
-            float: left;
-            display: block;
-            color: #f2f2f2;
-            text-align: center;
-            padding: 14px;
-            text-decoration: none;
+                float: left;
+                display: block;
+                color: #90ee90;
+                text-align: center;
+                padding: 14px;
+                text-decoration: none;
             }
 
             /* Page content */
             .content {
-            padding: 16px;
+                padding: 1.5rem;
             }
 
             /* The sticky class is added to the navbar with JS when it reaches its scroll position */
             .sticky {
-            position: fixed;
-            top: 0;
-            width: 100%;
+                position: fixed;
+                top: 0;
+                width: 100%;
             }
 
             /* Add some top padding to the page content to prevent sudden quick movement (as the navigation bar gets a new position at the top of the page (position:fixed and top:0) */
             .sticky + .content {
-            padding-top: 60px;
+                padding-top: 10px;
             }
             
             .vertical-center {
